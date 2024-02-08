af3acd895a02bfa3465b2e991da5f3dfa607061f
huan2935 <hanna.huang@mail.utoronto.ca>
2024-02-07 19:12:49 -0500

    Rewrote timer.py to be Nerve of Steel game

--- timer.py
+++ timer.py
@@ -6,18 +6,29 @@ timer.py uses the time library to help keep track of time
 """
 
 
-# This program is timer that counts down
+# This program is the Nerve of Steel Game. The last person to sit down before the timer runs out wins
 
-
+import random
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
 im = Image.open("times-up.jpeg")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
+while True:
+    ready_prompt = input("Are you ready to play Nerve of Steel? Y or N?")
 
-time.sleep(set_time)
+    if ready_prompt.upper() == "Y":
+        print("All players stand! Be the last person to sit down before the timer ends.")
+    
+# random time between 5 to 25 seconds
+        set_time = random.randint(5,5)
 
-im.show()
+        time.sleep(set_time)
+        im.show()
+        print("Times Up! The last person to sit down wins.")
 
+    else: 
+        print("Prepare for Nerve of Steel")
+
+
+    
